d5334e4a61c90a14c2be56c1db07a0c1726c7b28
Wilson Santos <contato@wilsonsantos.net>
2022-05-06 22:45:50 -0300

    :sparkles: Add Warrior class

---
+++ src/Entities/Warrior.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rpg.src.Entities
+{
+  public class Warrior : Hero
+  {
+
+
+    public Warrior(string Name, string CharacterClass = "Guerreiro", int Level = 0, string Weapon = "suas mãos") : base(Name, Level, CharacterClass, Weapon)
+    {
+      this.Name = Name;
+      this.Level = Level;
+      this.CharacterClass = CharacterClass;
+      this.Weapon = Weapon;
+    }
+
+
+  }
+}

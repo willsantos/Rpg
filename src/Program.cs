499c9e28f51b27c53a19a081ed81eda282d6afeb
Wilson Santos <contato@wilsonsantos.net>
2022-05-06 22:44:34 -0300

    :construction: Main game

---
+++ src/Program.cs
@@ -0,0 +1,22 @@
+﻿using Rpg.src.Entities;
+using static System.Console;
+
+namespace Rpg.src
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+
+      WriteLine("Qual o nome da sua personagem?");
+      string Name = ReadLine();
+
+
+      Warrior p1 = new Warrior(Name: Name);
+
+      WriteLine(p1.Attack());
+    }
+
+  }
+
+}

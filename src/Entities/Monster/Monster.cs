c7f148e2bc0faaea9e67274a0f1e8f790ac3db6f
Wilson Santos <contato@wilsonsantos.net>
2022-06-04 13:34:24 -0300

    :sparkles: feature: Add life status to Class Monster

--- src/Entities/Monster/Monster.cs
+++ src/Entities/Monster/Monster.cs
@@ -7,17 +7,20 @@ namespace Rpg.src.Entities.Monster
 {
   public abstract class Monster
   {
-    public Monster(string name, int Level, string weapon = "suas garras")
+    public Monster(string name, int level, int life, string weapon = "seu corpo")
     {
       this.Name = name;
-      this.Level = Level;
+      this.Level = level;
+      this.Life = life;
       this.Weapon = weapon;
 
     }
 
     public string Name { get; set; }
     public int Level { get; set; }
+    public int Life { get; set; }
     public string Weapon { get; set; }
+
 
     public virtual string Attack()
     {
